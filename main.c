94e04555db5c87dc3bff47140f175f98affec137
ilya-yurov <76982614+ilya-yurov@users.noreply.github.com>
2021-01-12 10:57:27 +0300

    Comments have been added

--- main.c
+++ main.c
@@ -8,37 +8,37 @@
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0])) // konstrukcia "sizeof(a) / sizeof((a)[0])
-                                                   // vichislyaet kollichestvo elementov massiva
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0])) // конструкция вида "sizeof(a) / sizeof((a)[0])"
+                                                   // вычисляет колличество элементов в массиве
 
-static uint8_t mode = SPI_CS_HIGH; // ustanavlivaem rezhim spi 0
-static uint8_t bits = 8;          // ustanavlivaem kollichestvo (bit v slove)
-static uint32_t speed = 500000;   // max skorost' peredachi
-static void pabort(const char *s) // vyvodim oshibku i abort v otdel'nuy funkciyu
+static uint8_t mode = SPI_CS_HIGH; // переменная устанавливает режим spi
+static uint8_t bits = 8;           // переменная устанавливает колличество бит
+static uint32_t speed = 500000;    // переменная устанавливает максимальную скорость передачи данных
+static void pabort(const char *s)  // функция pabort выполняет печать ошибки и выход из программы
 {
 	perror(s);
 	abort();
 }
-static void transfer(int fd) // funkciya polnodupleksnoy peredachi
+static void transfer(int fd) // реализация функции полнодюплексной передачи данных
 {
         int ret;
-        uint8_t tx[] = {              // eti dannye peredaem
+        uint8_t tx[] = {              // массив данных, которые необходимо передать
         0x48, 0x45, 0x4C, 0x4C, 0x4F, // ASCII chars "HELLO"
         0x20,                         // ASCII char " "
         0x57, 0x4F, 0x52, 0x4C, 0x44, // ASCII chars "WORLD"
         0x0A                          // ASCII char "\n"
         };
-        uint8_t rx[ARRAY_SIZE(tx)] = {0, };  // suda schityvaem
-        struct spi_ioc_transfer tr = {
-                .tx_buf = (unsigned long)tx, // ukazatel na bufer dannyh, kotorye nuzhno peredat' (libo NULL)
-                .rx_buf = (unsigned long)rx, // ukazatel na bufer dannyh, kotorye nuzhno schitat' (libo NULL)
-                .len = ARRAY_SIZE(tx),       // razmer buferov rx i tx v baytah
-                .delay_usecs = 0,        // vremya ozhidaniya v microsekundah
-                .speed_hz = 0,           // skorost' peredachi
-                .bits_per_word = bits,       // kollichestvo bit na slovo
+        uint8_t rx[ARRAY_SIZE(tx)] = {0, };  // массив данных, куда записываем данные
+        struct spi_ioc_transfer tr = {       //реализация transfer-структуры
+                .tx_buf = (unsigned long)tx, // указатель на буфер данных, которые необходимо передать
+                .rx_buf = (unsigned long)rx, // указатель на буфер данных, куда нужно писать
+                .len = ARRAY_SIZE(tx),       // размер буферов rx и tx в байтах
+                .delay_usecs = 0,            // врем ожидания в микросекундах
+                .speed_hz = 0,               // скорость передачи данных
+                .bits_per_word = bits,       // колличество бит в слове
         };
 
-        ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr); // peredacha. 1 v strukture. &tr - ukazatel na massiv structur
+        ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr); // передача сообщения через системный вызов ioctl(); &tr - указатель на массив данных
         if (ret < 1)
                 pabort("can't send spi message");
 }
@@ -62,23 +62,23 @@ int main()
         if (ret == -1)
                 pabort("can't get spi mode");
         /*
-         * bits per word
+         * бит в слове
          */
-        ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits); //peredaet ukazatel na bait, opredelyaushiy k>
+        ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits); // передает указатель на переменную, определяющую колличество бит для записи
         if (ret == -1)
                 pabort("can't set bits per word");
 
-        ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits); //to je na chtenie
+        ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits); // передает указатель на переменную, определяющую колличество бит для чтения
         if (ret == -1)
                 pabort("can't get bits per word");
 	/*
-         * max speed hz
+         * максимальная скорость передачи hz
          */
-        ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed); //ukazatel na peremennuy u32, kotoraya oprede>
+        ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed); // указатель на переменную u32, которая определяет максимальную скорость передачи данных (запись)
         if (ret == -1)
                 pabort("can't set max speed hz");
 
-        ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed); //to je na chtenie
+        ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed); // указатель на переменную u32, которая определяет максимальную скорость передачи данных (чтение)
         if (ret == -1)
                 pabort("can't get max speed hz");
 
